4eb7933b52c1ab8086fc0cba69d1b3cf13fda34a
jiangrenwen <351112802@qq.com>
2015-10-07 19:56:03 +0800

    更新菜单栏目

--- kpi/WebRoot/scripts/data.js
+++ kpi/WebRoot/scripts/data.js
@@ -2,45 +2,45 @@ var _menus = {
 	basic : [ {
 		"menuid" : "10",
 		"icon" : "icon-sys",
-		"menuname" : "年度计划指标",
+		"menuname" : "年度计划",
 		"menus" : [ {
 			"menuid" : "111",
-			"menuname" : "集团绩效考核指标",
+			"menuname" : "年度关键任务",
 			"icon" : "icon-nav",
 			"url" : "ptarget/groupkpi.html"
 		}, {
 			"menuid" : "113",
-			"menuname" : "部门绩效考核指标",
+			"menuname" : "部门年度计划",
 			"icon" : "icon-nav",
 			"url" : "ptarget/deptkpi.html"
 		}, {
 			"menuid" : "115",
-			"menuname" : "员工绩效考核指标",
+			"menuname" : "员工年度计划",
 			"icon" : "icon-nav",
 			"url" : "ptarget/employeekpi.html"
 		} ]
 	}, {
 		"menuid" : "20",
 		"icon" : "icon-sys",
-		"menuname" : "月度绩效考核指标",
+		"menuname" : "月度绩效",
 		"menus" : [ {
 			"menuid" : "211",
-			"menuname" : "制定部门月度考核方案",
+			"menuname" : "部门月度PBC",
 			"icon" : "icon-nav",
 			"url" : "pkpi/deptkpi.html"
 		}, {
 			"menuid" : "213",
-			"menuname" : "制定员工月度考核方案",
+			"menuname" : "员工月度PBC",
 			"icon" : "icon-nav",
 			"url" : "pkpi/employeekpi.html"
 		},{
 			"menuid" : "213",
-			"menuname" : "部门考核打分",
+			"menuname" : "部门月度评价",
 			"icon" : "icon-nav",
 			"url" : "pkpi/deptkpiScore.html"
 		},{
 			"menuid" : "213",
-			"menuname" : "员工考核打分",
+			"menuname" : "员工月度评价",
 			"icon" : "icon-nav",
 			"url" : "pkpi/employeekpiScore.html"
 		} ]
